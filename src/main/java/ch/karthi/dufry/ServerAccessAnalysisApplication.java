b880da27f2fde003f8d2349e95cb03606501ae52
karthikeyan <bgkarthikeyan@gmail.com>
2018-08-26 15:49:58 +0200

    Update ServerAccessAnalysisApplication.java

--- src/main/java/ch/karthi/dufry/ServerAccessAnalysisApplication.java
+++ src/main/java/ch/karthi/dufry/ServerAccessAnalysisApplication.java
@@ -5,16 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerAccessAnalysisApplication implements CommandLineRunner{
+public class ServerAccessAnalysisApplication{
 	
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerAccessAnalysisApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
 }
